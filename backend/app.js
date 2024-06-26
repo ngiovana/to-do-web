99dea0d5bb8e0fc6fd62d43e5fe3941d6177dec1
Giovana Niehues <giovana@bmasistemas.com.br>
2024-06-26 13:49:18 -0300

    feat: inicialização das rotas e controllers

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,16 @@ const express = require('express');
 // Inicializando a aplicação Express
 const app = express();
 
+// Importando as rotas da aplicação relacionadas às tarefas
+const taskRoutes = require('./routes/taskRoutes');
+
 // Definindo a porta em que o servidor irá escutar
 const PORT = process.env.PORT || 3000;
+
+// Configurando as rotas da aplicação para utilizar as rotas 
+//relacionadas às tarefas
+
+app.use('/api', taskRoutes);
 
 // Configurando uma rota inicial para exibir uma mensagem de boas-vindas
 app.get('/', (req, res) => {
